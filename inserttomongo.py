2e3c39016c410e9cc5044563e6f3709275145aef
ashishray94 <ashishray94@gmail.com>
2017-06-27 12:56:17 +0530

    ----

--- inserttomongo.py
+++ inserttomongo.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import json
 
-client = MongoClient(host='10.136.43.4', port=27017)
+client = MongoClient(host='127.0.0.1', port=27017)
 
 db = client['youtube_country_code']
 with open('data.json') as d:
